7f6d58477c3f92c73d45c9b8dec170aee95cc7f0
Odujole Ajibola <odujole.damilare@gmail.com>
2020-04-05 19:57:19 +0100

    Initial commit

---
+++ app/src/main/java/ng/com/ajsprojects/startngjavatask2/MainActivity.java
@@ -0,0 +1,16 @@
+package ng.com.ajsprojects.startngjavatask2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ng.com.ajsprojects.startngjavatask2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
